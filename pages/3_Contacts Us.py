16d7eb64a2c6b5d162598721ea996e7b86316f69
Klein-Baru <130561023+Klein-Baru@users.noreply.github.com>
2023-07-24 12:19:06 +0300

    edited code

--- pages/3_Contacts Us.py
+++ pages/3_Contacts Us.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title='KUISHI', page_icon='🏢', layout="centered", initial_sidebar_state="auto")
+st.success("This is a demo.")
+st.set_page_config(page_title='QWISHI', page_icon='🏢', layout="centered", initial_sidebar_state="auto")
 
 hide_streamlit_style = """
             <style>
@@ -10,7 +11,7 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-st.title("KUISHI")
+st.title("QWISHI")
 st.image("https://images.unsplash.com/photo-1596524430615-b46475ddff6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29udGFjdCUyMHVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60")
 st.subheader("We care about you, reach us using the following contacts ONLY:")
 
@@ -23,4 +24,4 @@ st.write("WhatsApp:")
 st.code("+254 714632602", language="python")
 
 st.write("📧Email:")
-st.code("kuishi@gmail.com", language="python")
+st.code("qwishi@gmail.com", language="python")
